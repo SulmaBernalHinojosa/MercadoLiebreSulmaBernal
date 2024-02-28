1444db75fbda13f521231937b86ace6a4907c755
Sulma Bernal <admin@ADMINs-MacBook-Air.local>
2024-02-27 23:54:34 -0400

    cambio a app

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
-index.use( express.static(publicPath) );
+app.use( express.static(publicPath) );
 
 
 const port = process.env.PORT || 3033;
-index.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`)
 });
 
-index.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });

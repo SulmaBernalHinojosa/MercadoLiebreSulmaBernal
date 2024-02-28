d76344d260a8bf1192b34c83fcc14dca69854598
Sulma Bernal <admin@ADMINs-MacBook-Air.local>
2024-02-27 23:46:20 -0400

    cambio a index

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
+index.use( express.static(publicPath) );
 
 
 const port = process.env.PORT || 3033;
-app.listen(port, () => {
+index.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`)
 });
 
-app.get('/', (req, res) => {
+index.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
